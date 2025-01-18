71378a3bae16af48f11859c58fe9347b2ad315c1
R_Q1nyu <2933779630@qq.com>
2025-01-19 01:40:00 +0800

    。

--- bili_server/nodes.py
+++ bili_server/nodes.py
@@ -105,19 +105,15 @@ class GraphNodes:
         keywords = state["input_keywords"]
         missing_keywords = [keyword for keyword in keywords if not state["keywords_in_rag"]]
         
-        for keyword in missing_keywords:
-            pass
-            #TODO: 执行检索
-            #documents =
-            #get_docs(missing_keywords, page=1)
-            from get_bilibili_data import get_data
-            documents=get_data.get(missing_keywords,page_num=1)
-            try:
-                print(f"检索到的文档为[前200字符]: {documents[:100]}")
-            except:
-                print()
-            #TODO: 向rag中添加
-            await self.rag.create_graph_store(documents)
+
+        from get_bilibili_data import get_data
+        documents=get_data.get(missing_keywords,page_num=1)
+        try:
+            print(f"检索到的文档为[前200字符]: {documents[:100]}")
+        except:
+            print()
+
+        DocumentLoader.get_instance().create_graph_store(documents)
         
         return state
 
